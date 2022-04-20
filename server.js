31c8fb4c0cb4daa5e964660270ef51681b61610f
nzwithee@gmail.com <Cartoon#01>
2022-04-19 20:13:28 -0500

    updates

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware
+app.use (express.urlencoded({ extended: false }));
+app.use (express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+
+        user: 'root',
+
+        password: 'mysqlpassword',
+
+        database: 'election'
+    },
+    
+    console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
