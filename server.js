dd9f36eda389ede334fc4df83b4c2b4691066f58
nzwithee@gmail.com <Cartoon#01>
2022-04-21 19:30:23 -0500

    updates

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ const db = mysql.createConnection(
 
 //Get all rows from the DB
 app.get('/api/candidates', (req, res)=>{
-    const sql = 'SELECT * FROM candidates';
+    const sql =`SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id` ;
 
     db.query(sql, (err, rows) => {
         if (err){
@@ -43,7 +47,13 @@ app.get('/api/candidates', (req, res)=>{
 
 //Get one row from the DB
 app.get('/api/candidate/:id', (req, res)=>{
-    const sql = 'SELECT * FROM candidates WHERE id = ?';
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;            
+    
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -103,6 +113,95 @@ app.post('/api/candidate', ({body}, res)=>{
             message: 'success',
             data: body
         });
+    });
+});
+
+//Update candidate
+app.put('/api/candidate/:id', (req, res)=> {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors){
+        res.status(400).json({error: errors});
+        return;
+    }
+    
+    const sql = 'UPDATE candidates SET party_id = ? WHERE id =?';
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql, params, (err, result)=>{
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        }
+        else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+//Party routes
+app.get('/api/parties', (req, res)=>{
+    const sql = 'SELECT * FROM parties';
+
+    db.query(sql, (err, rows)=>{
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//Party routes for one party
+app.get('/api/party/:id', (req, res)=>{
+    const sql = 'SELECT * FROM parties WHERE id =?';
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row)=>{
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//Delete row from parties
+app.delete('/api/party/:id', (req, res)=> {
+    const sql = 'DELETE FROM parties WHERE id =?';
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result)=>{
+        if(err){
+            res.status(400).json({error: err.message});
+        }
+        else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        }
+        else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
     });
 });
 
